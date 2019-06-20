baf689377a5e9c6ec092b49ac95bdf911944b28b
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2019-06-20 02:33:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ class Driver {
     this.name = name;
     this.startDate = new Date(startDate);
   }
+  yearsExperienceFromBeginningOff(year){
+    return year - this.startDate.getFullYear()
+  }
 }
